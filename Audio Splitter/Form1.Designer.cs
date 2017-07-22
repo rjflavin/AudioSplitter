2c83a633d3686f9cfc91aa14db593a7a14d8edcf
rjflavin <rjflavin@gmail.com>
2017-07-22 02:00:17 -0700

    Add files via upload
    Initial Upload.  Work in progress.

---
+++ Audio Splitter/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Audio_Splitter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputFileTextArea = new System.Windows.Forms.TextBox();
+            this.outputDirectoryTextArea = new System.Windows.Forms.TextBox();
+            this.InputFileLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.InputFileButton = new System.Windows.Forms.Button();
+            this.OutputDirectoryButton = new System.Windows.Forms.Button();
+            this.SplitButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // inputFileTextArea
+            // 
+            this.inputFileTextArea.Location = new System.Drawing.Point(115, 12);
+            this.inputFileTextArea.Name = "inputFileTextArea";
+            this.inputFileTextArea.ReadOnly = true;
+            this.inputFileTextArea.Size = new System.Drawing.Size(374, 20);
+            this.inputFileTextArea.TabIndex = 0;
+            // 
+            // outputDirectoryTextArea
+            // 
+            this.outputDirectoryTextArea.Location = new System.Drawing.Point(115, 44);
+            this.outputDirectoryTextArea.Name = "outputDirectoryTextArea";
+            this.outputDirectoryTextArea.Size = new System.Drawing.Size(374, 20);
+            this.outputDirectoryTextArea.TabIndex = 1;
+            // 
+            // InputFileLabel
+            // 
+            this.InputFileLabel.AutoSize = true;
+            this.InputFileLabel.Location = new System.Drawing.Point(27, 14);
+            this.InputFileLabel.Name = "InputFileLabel";
+            this.InputFileLabel.Size = new System.Drawing.Size(53, 13);
+            this.InputFileLabel.TabIndex = 2;
+            this.InputFileLabel.Text = "Input File:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Output Directory:";
+            // 
+            // InputFileButton
+            // 
+            this.InputFileButton.Location = new System.Drawing.Point(496, 9);
+            this.InputFileButton.Name = "InputFileButton";
+            this.InputFileButton.Size = new System.Drawing.Size(102, 23);
+            this.InputFileButton.TabIndex = 4;
+            this.InputFileButton.Text = "Browse";
+            this.InputFileButton.UseVisualStyleBackColor = true;
+            this.InputFileButton.Click += new System.EventHandler(this.InputFileButton_Click);
+            // 
+            // OutputDirectoryButton
+            // 
+            this.OutputDirectoryButton.Location = new System.Drawing.Point(496, 41);
+            this.OutputDirectoryButton.Name = "OutputDirectoryButton";
+            this.OutputDirectoryButton.Size = new System.Drawing.Size(102, 23);
+            this.OutputDirectoryButton.TabIndex = 5;
+            this.OutputDirectoryButton.Text = "Browse";
+            this.OutputDirectoryButton.UseVisualStyleBackColor = true;
+            this.OutputDirectoryButton.Click += new System.EventHandler(this.OutputDirectoryButton_Click);
+            // 
+            // SplitButton
+            // 
+            this.SplitButton.Location = new System.Drawing.Point(15, 305);
+            this.SplitButton.Name = "SplitButton";
+            this.SplitButton.Size = new System.Drawing.Size(583, 42);
+            this.SplitButton.TabIndex = 7;
+            this.SplitButton.Text = "Split Files";
+            this.SplitButton.UseVisualStyleBackColor = true;
+            this.SplitButton.Click += new System.EventHandler(this.SplitButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 73);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(525, 226);
+            this.panel1.TabIndex = 8;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(519, 223);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(553, 73);
+            this.trackBar1.Maximum = 50;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 219);
+            this.trackBar1.TabIndex = 9;
+            this.trackBar1.Value = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 355);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.SplitButton);
+            this.Controls.Add(this.OutputDirectoryButton);
+            this.Controls.Add(this.InputFileButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.InputFileLabel);
+            this.Controls.Add(this.outputDirectoryTextArea);
+            this.Controls.Add(this.inputFileTextArea);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Audio Splitter";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputFileTextArea;
+        private System.Windows.Forms.TextBox outputDirectoryTextArea;
+        private System.Windows.Forms.Label InputFileLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button InputFileButton;
+        private System.Windows.Forms.Button OutputDirectoryButton;
+        private System.Windows.Forms.Button SplitButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ Audio Splitter/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using NAudio.Wave;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Drawing;
+using System.IO;
+
+namespace Audio_Splitter
+{
+    public partial class Form1 : Form
+    {
+        List<int> clippedFileStartPositions = new List<int>();
+        List<int> clippedFileEndPositions = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void InputFileButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                inputFileTextArea.Text = openFileDialog1.FileName;
+            }
+
+            graphFile(inputFileTextArea.Text);
+        }
+
+        private void CreateSaveBitmap(Canvas canvas, string filename)
+        {
+            RenderTargetBitmap renderBitmap = new RenderTargetBitmap(
+             (int)canvas.Width, (int)canvas.Height,
+             96d, 96d, PixelFormats.Pbgra32);
+            // needed otherwise the image output is black
+            canvas.Measure(new System.Windows.Size((int)canvas.Width, (int)canvas.Height));
+            canvas.Arrange(new Rect(new System.Windows.Size((int)canvas.Width, (int)canvas.Height)));
+
+            renderBitmap.Render(canvas);
+
+            //JpegBitmapEncoder encoder = new JpegBitmapEncoder();
+            PngBitmapEncoder encoder = new PngBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(renderBitmap));
+            pictureBox1.Width = renderBitmap.PixelWidth;
+            pictureBox1.Image = BitmapFromSource(renderBitmap);
+
+            using (FileStream file = File.Create(filename))
+            {
+                encoder.Save(file);
+            }
+        }
+
+        private Bitmap BitmapFromSource(BitmapSource bitmapsource)
+        {
+            Bitmap bitmap;
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+
+                enc.Frames.Add(BitmapFrame.Create(bitmapsource));
+                enc.Save(outStream);
+                bitmap = new Bitmap(outStream);
+            }
+            return bitmap;
+        }
+
+        private void graphFile(string file)
+        {
+            var window = new Window();
+            var canvas = new Canvas();
+
+            //WaveFormat target = new WaveFormat(8000, 8, 1);
+            //WaveStream stream = new WaveFileReader("c:\\test.wav");
+            //WaveFormatConversionStream str = new WaveFormatConversionStream(target, stream);
+            //WaveFileWriter.CreateWaveFile("c:\\converted.wav", str);
+
+            using (var reader = new AudioFileReader(@file))
+            {
+                var samples = (reader.Length) / (reader.WaveFormat.Channels * reader.WaveFormat.BitsPerSample / 8);
+                var f = 0.0f;
+                var max = 0.0f;
+                // waveform will be a maximum of 4000 pixels wide:
+                var batch = (int)Math.Max(40, samples / 4000)/4;
+                var mid = 100;
+                var yScale = 100;
+                float[] buffer = new float[batch];
+                int read;
+                var xPos = 0;
+
+                Boolean insideFile = false;
+                int[] startEndPositions = { 0,0};
+                //System.Windows.Forms.MessageBox.Show(batch.ToString());
+                while ((read = reader.Read(buffer, 0, batch)) == batch)
+                {
+                    for (int n = 0; n < read; n++)
+                    {
+                        max = Math.Max(Math.Abs(buffer[n]), max);
+                    }
+                    
+                    var line = new System.Windows.Shapes.Line();
+                    line.X1 = xPos;
+                    line.X2 = xPos;
+                    line.Y1 = mid + (max * yScale);
+                    line.Y2 = mid - (max * yScale);
+                    line.StrokeThickness = 1;
+
+                    //Console.WriteLine("Y1:" + line.Y1);
+                    //Console.WriteLine("Y2:" + line.Y2);
+
+                    if (line.Y1 < 100+trackBar1.Value && line.Y2>trackBar1.Value)
+                    {
+                        line.Stroke = System.Windows.Media.Brushes.Black;
+
+                        if(insideFile==true)
+                        {
+                            insideFile = false;
+                            startEndPositions[1] = (int)reader.Position;
+                            clippedFileEndPositions.Add(startEndPositions[1]);
+                            //Console.WriteLine(startEndPositions[0]+","+startEndPositions[1]);
+                        }
+                    }
+                    else
+                    {
+                        line.Stroke = System.Windows.Media.Brushes.Green;
+
+                        if(insideFile==false)
+                        {
+                            startEndPositions[0] = (int)reader.Position;
+                            clippedFileStartPositions.Add(startEndPositions[0]);
+                            insideFile = true;
+                        }
+                    }
+                    canvas.Children.Add(line);
+                    max = 0;
+                    xPos++;
+                }
+                canvas.Width = xPos;
+                canvas.Height = mid * 2;
+            }
+            window.Height = 260;
+            var scrollViewer = new ScrollViewer();
+            scrollViewer.Content = canvas;
+            scrollViewer.HorizontalScrollBarVisibility = ScrollBarVisibility.Auto;
+            window.Content = scrollViewer;
+
+            //window.ShowDialog();
+
+            CreateSaveBitmap(canvas, "test");
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            graphFile(inputFileTextArea.Text);
+        }
+
+        private void OutputDirectoryButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog openFileDialog1 = new FolderBrowserDialog();
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                outputDirectoryTextArea.Text = openFileDialog1.SelectedPath;
+            }
+        }
+
+        private void SplitButton_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(outputDirectoryTextArea.Text) && File.Exists(inputFileTextArea.Text))
+            {
+
+                    for (int i = 0; i < clippedFileStartPositions.Count; i++)
+                    {
+                        using (AudioFileReader reader = new AudioFileReader(inputFileTextArea.Text))
+                        {
+                            using (WaveFileWriter writer = new WaveFileWriter(outputDirectoryTextArea.Text + @"\" + Path.GetFileNameWithoutExtension(inputFileTextArea.Text) + i + ".wav", reader.WaveFormat))
+                            {
+                                Console.WriteLine(outputDirectoryTextArea.Text + @"\" + Path.GetFileNameWithoutExtension(inputFileTextArea.Text) + i + ".wav");
+                                //Console.WriteLine(clippedFileStartEndPositionsList[i][0] + "," + clippedFileStartEndPositionsList[i][1]);
+                                TrimWavFile(reader, writer, clippedFileStartPositions[i], clippedFileEndPositions[i]);
+                            }
+                        }
+                    }
+                
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("Please Select a Valid Input File and Output Directory");
+            }
+}
+
+        private static void TrimWavFile(AudioFileReader reader, WaveFileWriter writer, int startPos, int endPos)
+        {
+            reader.Position = startPos;
+            byte[] buffer = new byte[1024];
+            while (reader.Position < endPos)
+            {
+                //Console.WriteLine(reader.Position);
+                //Console.WriteLine(endPos);
+                int bytesRequired = (int)(endPos - reader.Position);
+                if (bytesRequired > 0)
+                {
+                    int bytesToRead = Math.Min(bytesRequired, buffer.Length);
+                    int bytesRead = reader.Read(buffer, 0, bytesToRead);
+                    if (bytesRead > 0)
+                    {
+                        writer.Write(buffer, 0, bytesRead);
+                    }
+                }
+            }
+        }
+    }
+}
